553efbef5440713ce404e5c04b15f20491cd20e4
David Hempston <cyber_veio@hotmail.com>
2023-09-04 14:21:57 +0100

    Grab token

--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -8,4 +8,6 @@ then
         -u $INFLUXDB_USER \
         -p $INFLUXDB_PASSWORD \
         -b $INFLUXDB_BUCKET
+    influx auth create --user $INFLUXDB_USER --org $INFLUXDB_BUCKET --read-buckets --write-buckets
+    export INFLUXDB_TOKEN=$(influx auth list | grep $INFLUXDB_USER | grep -o "[[:alnum:]\_\-]*==")
 fi
